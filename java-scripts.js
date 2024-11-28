c51c4736edef7ebba94af9c6dcfaf3f07e43e343
Lance-pixel-dot <renierlawrencetuico143@gmail.com>
2024-11-28 18:20:55 +0800

    Add HTML
    Added html to create a User Interface for the game and made some changes on javascript

--- java-scripts.js
+++ java-scripts.js
@@ -1,72 +1,105 @@
 let humanScore = 0
 let computerScore = 0
 
+const choice = document.querySelector('#choices');
+const humanSlct = document.querySelector('#humanSlct');
+const computerSlct = document.querySelector('#computerSlct');
+const roundResult = document.querySelector('#round');
+const humanScr = document.querySelector('#humanScr');
+const computerScr = document.querySelector('#computerScr');
+const result = document.querySelector('#winner');
+
+let playerSelection;
+let rounds = 0;
+let roundLimit = 5;
+
+
 function getComputerChoice(){
    return Math.floor(Math.random() * 3)
-}
-
-function getHumanChoice(){
-   return prompt("Enter you input").toLowerCase()
 }
 
 function playRound(humanChoice, computerChoice){
 
    computerChoice = getComputerChoice()
-   humanChoice = getHumanChoice()
+   humanChoice = playerSelection;
+
+   humanSlct.textContent = ` ${playerSelection}`;
 
     if(computerChoice === 0){
+
         computerChoice = 'rock'
+        computerSlct.textContent = ` ${computerChoice}`;
+
         if(humanChoice === 'scissor'){
             computerScore++
-            return 'You lose!'
+            roundResult.textContent = `You lose!`;
         }else if(humanChoice === computerChoice){
-            return 'Draw!'
+            roundResult.textContent = 'Draw!';
         }else{
             humanScore++
-            return 'You win!'
+            roundResult.textContent = 'You win!';
         }
+                
     }else if(computerChoice === 1){
+
         computerChoice = 'paper'
+        computerSlct.textContent = ` ${computerChoice}`;
+
         if(humanChoice === 'rock'){
             computerScore++
-            return 'You lose!'
+            roundResult.textContent = 'You lose!';
         }else if(humanChoice === computerChoice){
-            return 'Draw!'
+            roundResult.textContent = 'Draw!';
         }else{
             humanScore++
-            return 'You win!'
+            roundResult.textContent = 'You win!';
         }
+
     }else if(computerChoice === 2){
+
         computerChoice = 'scissor'
+        computerSlct.textContent = ` ${computerChoice}`;
+
         if(humanChoice === 'paper'){
             computerScore++
-            return 'You lose!'
+            roundResult.textContent = 'You lose!';
         }else if(humanChoice === computerChoice){
-            return 'Draw!'
+            roundResult.textContent = 'Draw!';
         }else{
             humanScore++
-            return 'You win!'
+            roundResult.textContent = 'You win!';
         }
+
     }
 
+    humanScr.textContent = humanScore;
+    computerScr.textContent = computerScore;
 }
 
 function playGame(){
-    console.log('round 1: ')
-    console.log(playRound())
-    console.log('round 2: ')
-    console.log(playRound())
-    console.log('round 3: ')
-    console.log(playRound())
-    console.log('round 4: ')
-    console.log(playRound())
-    console.log('round 5: ')
-    console.log(playRound())
-    console.log('Scores: ')
-    console.log(`Human Score: ${humanScore}`)
-    console.log(`Computer Score: ${computerScore}`)
 
-    return 'Gamer Over!'
+    choice.addEventListener('click', (e) => {
+    let target = e.target;
+
+    switch(target.className){
+
+       case 'rock':
+           playerSelection = 'rock'
+           playRound();
+           break;
+       case 'paper':
+           playerSelection = 'paper'
+           playRound();
+           break;
+       case 'scissor':
+           playerSelection = 'scissor'
+           playRound();
+           break;
+
+    }
+
+   });
+
 }
 
-console.log(playGame())
+console.log(playGame());
