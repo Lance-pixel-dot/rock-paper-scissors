d91c76ee4caa8e1fec311f5978dd62378fdd17d5
Lance-pixel-dot <renierlawrencetuico143@gmail.com>
2024-11-30 16:51:41 +0800

    Finish UI
    Everything is done fixed all bugs and the UI is complete

--- java-scripts.js
+++ java-scripts.js
@@ -12,15 +12,20 @@ const currentRnd = document.querySelector('#round');
 const start = document.querySelector('#start');
 const restart = document.querySelector('#restart');
 const continyu = document.querySelector('#continue');
-const placeHolderImage = document.querySelector('.placeHolder');
-const rock = document.querySelector('.rock');
-const paper = document.querySelector('.paper');
-const scissor = document.querySelector('.scissor');
+const hRock = document.querySelector('.hRock');
+const hPaper = document.querySelector('.hPaper');
+const hScissor = document.querySelector('.hScissor');
+const cRock = document.querySelector('.cRock');
+const cPaper = document.querySelector('.cPaper');
+const cScissor = document.querySelector('.cScissor');
+const hPlaceHolder = document.querySelector('.hPlaceHolder');
+const cPlaceHolder = document.querySelector('.cPlaceHolder');
+const hSContainer = document.querySelector('#hum-container');
+const cSContainer = document.querySelector('#comp-container');
 
 let playerSelection;
 let rounds = 1;
 let roundLimit = 6;
-
 
 function getComputerChoice(){
    return Math.floor(Math.random() * 3)
@@ -28,66 +33,62 @@ function getComputerChoice(){
 
 function playRound(humanChoice, computerChoice){
 
+   hPlaceHolder.style.display = 'none';
+   cPlaceHolder.style.display = 'none';
+
    computerChoice = getComputerChoice()
    humanChoice = playerSelection;
 
-   currentRnd.textContent = rounds;
+   currentRnd.textContent = `Round ${rounds}`;
 
-   humanSlct.textContent = `${playerSelection}`;
+   humanSlct.textContent = `${playerSelection.toUpperCase()}`;
 
-    if(computerChoice === 0){
+    if(computerChoice === 0){     
 
+        cRock.style.display = 'block';
         computerChoice = 'rock';
-        computerSlct.textContent = `${computerChoice}`;
-        rock.style.display = 'block';
-        placeHolderImage.style.display = 'none';
+        computerSlct.textContent = `${computerChoice.toUpperCase()}`;
 
         if(humanChoice === 'scissor'){
-            scissor.style.display = 'block';
-            computerScore++
-            roundResult.textContent = `You lose!`;
+            computerScore++;
+            roundResult.textContent = `computer +1!`;
         }else if(humanChoice === computerChoice){
-            rock.style.display = 'block';
             roundResult.textContent = 'Draw!';
         }else{
             humanScore++
-            roundResult.textContent = 'You win!';
+            roundResult.textContent = 'You +1!';
         }
                 
     }else if(computerChoice === 1){
 
-        computerChoice = 'paper'
-        computerSlct.textContent = `${computerChoice}`;
-        paper.style.display = 'block'
-        placeHolderImage.style.display = 'none';
+        cPaper.style.display = 'block';
+        computerChoice = 'paper';
+        computerSlct.textContent = `${computerChoice.toUpperCase()}`;
 
         if(humanChoice === 'rock'){
-            rock.style.display = 'block';
-            computerScore++ 
-            roundResult.textContent = 'You lose!';
+            computerScore++
+            roundResult.textContent = 'computer +1!';
         }else if(humanChoice === computerChoice){
             roundResult.textContent = 'Draw!';
         }else{
             humanScore++
-            roundResult.textContent = 'You win!';
+            roundResult.textContent = 'You +1!';
         }
 
     }else if(computerChoice === 2){
 
-        scissor.style.display = 'block';
-        placeHolderImage.style.display = 'none';
-        computerChoice = 'scissor'
-        computerSlct.textContent = `${computerChoice}`;
+        cScissor.style.display = 'block';
+        computerChoice = 'scissor';
+        computerSlct.textContent = `${computerChoice.toUpperCase()}`;
 
         if(humanChoice === 'paper'){
-            paper.style.display = 'block';
             computerScore++
-            roundResult.textContent = 'You lose!';
+            roundResult.textContent = 'computer +1!';
         }else if(humanChoice === computerChoice){
             roundResult.textContent = 'Draw!';
         }else{
             humanScore++
-            roundResult.textContent = 'You win!';
+            roundResult.textContent = 'You +1!';
         }
 
     }
@@ -104,7 +105,16 @@ function playRound(humanChoice, computerChoice){
         humanSlct.textContent = null;
         computerSlct.textContent = null;
         roundResult.textContent = null;
-        currentRnd.textContent = rounds;
+        currentRnd.textContent = `Round ${rounds}`;
+        hPlaceHolder.style.display = 'block';
+        cPlaceHolder.style.display = 'block';
+        hRock.style.display = 'none';
+        hPaper.style.display = 'none';
+        hScissor.style.display = 'none';
+        cPaper.style.display = 'none';
+        cRock.style.display = 'none';
+        cScissor.style.display = 'none';
+
         gameResult();
     });
     
@@ -119,14 +129,17 @@ function playGame(){
     switch(target.className){
 
        case 'gameButton rock':
+           hRock.style.display = 'block';
            playerSelection = 'rock'
            playRound();
            break;
        case 'gameButton paper':
+           hPaper.style.display = 'block';
            playerSelection = 'paper'
            playRound();
            break;
        case 'gameButton scissor':
+           hScissor.style.display = 'block';
            playerSelection = 'scissor'
            playRound();
            break;
@@ -144,7 +157,9 @@ function gameMenu(){
     start.addEventListener('click', () => {
         choice.style.display = 'block';
         start.style.display = 'none';
-        currentRnd.textContent = rounds;
+        currentRnd.textContent = `Round ${rounds}`;
+        hSContainer.style.display = 'block';
+        cSContainer.style.display = 'block';
         playGame();
     });
 
@@ -159,9 +174,9 @@ function gameMenu(){
 
         humanSlct.textContent = '';
         computerSlct.textContent = '';
-        humanScr.textContent = null;
-        computerScr.textContent = null;
-        currentRnd.textContent = rounds;
+        humanScr.textContent = humanScore;
+        computerScr.textContent = computerScore;
+        currentRnd.textContent = `Round ${rounds}`;
         roundResult.textContent = '';
         result.textContent = '';
     })
@@ -170,18 +185,21 @@ function gameMenu(){
 function gameResult(){
 
     if(rounds === roundLimit && computerScore > humanScore){
+        result.style.display = 'block';
         result.textContent = 'Computer Wins!';
         currentRnd.textContent = 'Game Finish!';
         choice.style.display = 'none';
         restart.style.display = 'block';
         gameMenu();
     }else if(rounds === roundLimit && humanScore > computerScore){
+        result.style.display = 'block';
         result.textContent = 'You Win!';
         currentRnd.textContent = 'Game Finish!';
         choice.style.display = 'none';
         restart.style.display = 'block';
         gameMenu();
     }else if(rounds === roundLimit && humanScore === computerScore){
+        result.style.display = 'block';
         result.textContent = 'Tie no Winner!';
         currentRnd.textContent = 'Game Finish!';
         choice.style.display = 'none';
